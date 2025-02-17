88cb400191f6af5f5830ea718b7ebf449a9161d0
Sudeeparyan <sudeeparyang@gmail.com>
2025-02-17 14:20:51 +0530

    panda

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // This tells Vite to build using relative paths
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
